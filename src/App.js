5df5a327f13cb5384dfe83cb3f76ba778f131ced
Natalia Suarez <nataliasuarez@Natalias-MacBook-Pro.local>
2021-10-11 13:45:08 -0300

    Add images with search form using Formik library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import "./header.css";
+import "./content.css";
+import "./article.css";
 
-function App() {
+const App = () => {
+  const [photos, setPhotos] = useState([]);
+  const open = (url) => window.open(url);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: "" }}
+          onSubmit={async (values) => {
+            const response = await fetch(
+              `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+              {
+                headers: {
+                  // SET your API KEY of unsplash.com/developers in .env file
+                  Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_API_KEY}`,
+                },
+              }
+            );
+            const data = await response.json();
+            if (data.results) setPhotos(data.results);
+          }}
         >
-          Learn React
-        </a>
+          <Form style={{ width: "100%" }}>
+            <Field name="search" placeholder="Search an image" />
+          </Form>
+        </Formik>
       </header>
+      <div className="container">
+        <div className="center">
+          {photos.map((photo) => (
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular} alt={photo.alt_description} />
+              <p>{[photo.description, photo.alt_description].join(" -")}</p>
+            </article>
+          ))}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
